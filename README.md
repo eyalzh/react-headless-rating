02b92c15d58766c095d60eef06658010195dc3b9
Eyal Zach <eyalzh@gmail.com>
2023-11-10 13:02:52 +0200

    basic functionality

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # React Headless Rating component
 
-Fully unstyled, accessable, rating react component.
+Fully unstyled, accessable, rating component.
+
+## Features
+
+- Use any react element (e.g. SVG) to represent a "star"
+- Each element has three states that can be fully styled: Inactive, Active, Selected
+- WCAG compliant (including keyboard activation)
+
+## Demo
+
+## Install
+
+```bash
+npm install react-headless-rating
+```
+
+```bash
+yarn add react-headless-rating
+```
---
+++ src/index.tsx
@@ -0,0 +1,131 @@
+import React, {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useId,
+} from "react";
+
+export type StarState = "selected" | "unselected";
+
+interface BasicHeadlessComponentProps extends PropsWithChildren {
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+interface RatingContainerProps extends BasicHeadlessComponentProps {
+  currentRating?: number | null;
+  onChangeRating?: (newRating: number | null) => void;
+  readonly?: boolean;
+}
+
+interface StarProps extends Omit<BasicHeadlessComponentProps, "className"> {
+  label: string;
+  className?: string | ((state: StarState, readonly: boolean) => string);
+}
+
+interface RatingContext {
+  readonly currentRating: number | null | undefined;
+  readonly currentSelection: number | null;
+  readonly readonly: boolean | undefined;
+  setCurrentRating: (newRating: number | null) => void;
+  setCurrentSelection: (newSelection: number | null) => void;
+}
+
+const RatingContext = createContext<RatingContext | null>(null);
+const StarContext = createContext<number | null>(null);
+
+function RatingContainer(props: RatingContainerProps) {
+  const [currentSelection, setCurrentSelection] = React.useState<number | null>(
+    null
+  );
+
+  const ratingContext: RatingContext = {
+    currentRating: props.currentRating,
+    currentSelection,
+    readonly: props.readonly,
+    setCurrentRating: (newRating) => {
+      props.onChangeRating && props.onChangeRating(newRating);
+    },
+    setCurrentSelection: (newSelection) => {
+      setCurrentSelection(newSelection);
+    },
+  };
+
+  const starChildren = React.Children.map(props.children, (child, i) => {
+    return <StarContext.Provider value={i + 1}>{child}</StarContext.Provider>;
+  });
+
+  return (
+    <RatingContext.Provider value={ratingContext}>
+      <div className={props.className} style={props.style}>
+        {starChildren}
+      </div>
+    </RatingContext.Provider>
+  );
+}
+
+function Star(props: StarProps) {
+  const context = useContext(RatingContext);
+  const starIndex = useContext(StarContext);
+  const [starState, setStarState] = React.useState<StarState>("unselected");
+
+  const id = useId();
+  const fullInternalId = `__hr__star-${id}`;
+
+  const onClick = () => {
+    !context?.readonly && context?.setCurrentRating(starIndex);
+  };
+
+  let finalClassName;
+  if (typeof props.className === "function") {
+    finalClassName = props.className(starState, !!context?.readonly);
+  } else {
+    finalClassName = props.className;
+  }
+
+  useEffect(() => {
+    if (
+      (context?.currentSelection ?? context?.currentRating ?? 0) >=
+      (starIndex ?? 0)
+    ) {
+      setStarState("selected");
+    } else {
+      setStarState("unselected");
+    }
+  }, [
+    context?.currentRating,
+    starIndex,
+    setStarState,
+    context?.currentSelection,
+  ]);
+
+  const onMouseEnter = () => {
+    !context?.readonly && context?.setCurrentSelection(starIndex);
+  };
+
+  const onMouseOut = () => {
+    !context?.readonly && context?.setCurrentSelection(null);
+  };
+
+  return (
+    <div
+      role="radio"
+      aria-label={props.label}
+      aria-checked={starState === "selected"}
+      aria-readonly={context?.readonly ? "true" : "false"}
+      id={fullInternalId}
+      className={finalClassName}
+      style={props.style}
+      onClick={onClick}
+      onMouseEnter={onMouseEnter}
+      onMouseOut={onMouseOut}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export const Rating = Object.assign(RatingContainer, {
+  Star,
+});
